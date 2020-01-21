95dc084d2de461781b218b1d9aea21e115f24bbe
Aleksanteri Heliövaara <aleksanteriheliovaara@aleksanterismbp.lan>
2020-01-21 14:43:01 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,44 @@ import Hero from '../components/Hero';
 import theme from "../theme";
 import { ThemeProvider } from 'styled-components';
 import Container from '../components/Container';
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+    body, html {
+        margin: 0px;
+        padding: 0px;
+        max-width: 100%;
+        height: 100%;
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+    h1 {
+        margin: 0
+    }
+    img {
+        max-width: 100%;
+    }
+    a {
+        all: unset;
+    }
+    a:link {
+        all: unset;
+    }
+    a:focus {
+        all: unset;
+    }
+    a:active {
+        all: unset;
+    }
+    a:visited {
+        all: unset;
+    }
+    a:hover {
+        all: unset;
+    }
+    button {
+        all: unset;
+    }
+`;
 const Home = () => {
 
 	const [productsList, setProducts] = useState([])
@@ -27,6 +64,7 @@ const Home = () => {
 				<Navigation />
 				{Products(productsList)}
 			</Container>
+			<GlobalStyle />
 		</ThemeProvider>
 	)
 }
